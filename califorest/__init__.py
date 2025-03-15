fdb451b8702fae50747dd058d59e433205728a80
Pranav Herur <herur.pranav@gmail.com>
2025-03-14 22:48:53 -1000

    first attempt at improvement

--- califorest/__init__.py
+++ califorest/__init__.py
@@ -1,4 +1,4 @@
-from .califorest import CaliForest
+from .califorest import CaliForest, ImprovedCaliForest
 from .rc30 import RC30
 
-__all__ = ["CaliForest", "RC30"]
+__all__ = ["CaliForest", "RC30", "ImprovedCaliForest"]
