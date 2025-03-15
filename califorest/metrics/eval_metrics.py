af51c06d0d995f241279596985441cdd52aa04a9
Pranav Herur <pranav.herur@podium.com>
2025-03-14 20:38:54 -1000

    working version: can run run_example on python3.10

--- califorest/metrics/eval_metrics.py
+++ califorest/metrics/eval_metrics.py
@@ -4,6 +4,7 @@ import sklearn.metrics as skm
 from scipy.stats import chi2
 from scipy.stats import norm
 
+
 def hosmer_lemeshow(y_true, y_score):
     """
     Calculate the Hosmer Lemeshow to assess whether
@@ -13,53 +14,52 @@ def hosmer_lemeshow(y_true, y_score):
     Assume that there are 10 groups:
     HL = \\sum_{g=1}^G \\frac{(O_{1g} - E_{1g})^2}{N_g \\pi_g (1- \\pi_g)}
     """
-    n_grp = 10 # number of groups
+    n_grp = 10  # number of groups
 
     # create the dataframe
-    df = pd.DataFrame({'score': y_score, 'target': y_true})
+    df = pd.DataFrame({"score": y_score, "target": y_true})
 
     # sort the values
-    df = df.sort_values('score')
+    df = df.sort_values("score")
     # shift the score a bit
-    df['score'] = np.clip(df['score'], 1e-8, 1-1e-8)
-    df['rank'] = list(range(df.shape[0]))
+    df["score"] = np.clip(df["score"], 1e-8, 1 - 1e-8)
+    df["rank"] = list(range(df.shape[0]))
     # cut them into 10 bins
-    df['score_decile'] = pd.qcut(df['rank'], n_grp,
-                                      duplicates='raise')
+    df["score_decile"] = pd.qcut(df["rank"], n_grp, duplicates="raise")
     # sum up based on each decile
-    obsPos = df['target'].groupby(df.score_decile).sum()
-    obsNeg = (df['target'].groupby(df.score_decile).count() - 
-                obsPos)
-    exPos = df['score'].groupby(df.score_decile).sum()
-    exNeg = df['score'].groupby(df.score_decile).count() - exPos
-    hl = (((obsPos - exPos)**2/exPos) + ((obsNeg - exNeg)**2/exNeg)).sum()
+    obsPos = df["target"].groupby(df.score_decile).sum()
+    obsNeg = df["target"].groupby(df.score_decile).count() - obsPos
+    exp = df["score"].groupby(df.score_decile, observed=True).sum()
+    exNeg = df["score"].groupby(df.score_decile, observed=True).count() - exp
+    hl = (((obsPos - exp) ** 2 / exp) + ((obsNeg - exNeg) ** 2 / exNeg)).sum()
 
     # https://en.wikipedia.org/wiki/Pearson%27s_chi-squared_test
     # Re: p-value, higher the better Goodness-of-Fit
-    p_value = 1 - chi2.cdf(hl, n_grp-2)
-    
+    p_value = 1 - chi2.cdf(hl, n_grp - 2)
+
     return p_value
+
 
 def reliability(y_true, y_score):
 
     n_grp = 10
-    df = pd.DataFrame({'score': y_score, 'target': y_true})
-    df = df.sort_values('score')
-    df['rank'] = list(range(df.shape[0]))
-    df['score_decile'] = pd.qcut(df['rank'], n_grp,
-                                      duplicates='raise')
+    df = pd.DataFrame({"score": y_score, "target": y_true})
+    df = df.sort_values("score")
+    df["rank"] = list(range(df.shape[0]))
+    df["score_decile"] = pd.qcut(df["rank"], n_grp, duplicates="raise")
 
-    obs = df['target'].groupby(df.score_decile).mean()
-    exp = df['score'].groupby(df.score_decile).mean()
+    obs = df["target"].groupby(df.score_decile).mean()
+    exp = df["score"].groupby(df.score_decile, observed=True).mean()
 
-    rel_small = np.mean((obs - exp)**2)
-    rel_large = (np.mean(y_true) - np.mean(y_score))**2
+    rel_small = np.mean((obs - exp) ** 2)
+    rel_large = (np.mean(y_true) - np.mean(y_score)) ** 2
 
     return rel_small, rel_large
 
+
 def spiegelhalter(y_true, y_score):
-    top = np.sum((y_true - y_score)*(1-2*y_score))
-    bot = np.sum((1-2*y_score)**2 * y_score * (1-y_score))
+    top = np.sum((y_true - y_score) * (1 - 2 * y_score))
+    bot = np.sum((1 - 2 * y_score) ** 2 * y_score * (1 - y_score))
     sh = top / np.sqrt(bot)
 
     # https://en.wikipedia.org/wiki/Z-test
@@ -69,9 +69,10 @@ def spiegelhalter(y_true, y_score):
 
     return p_value
 
+
 def scaled_brier_score(y_true, y_score):
     brier = skm.brier_score_loss(y_true, y_score)
     # calculate the mean of the probability
-    p = np.mean(y_true)  
-    brier_scaled = 1 - brier / (p * (1-p))
+    p = np.mean(y_true)
+    brier_scaled = 1 - brier / (p * (1 - p))
     return brier, brier_scaled
--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(packages=find_packages(),
+setup(
+    packages=find_packages(),
     name="califorest",
     version="0.0.1",
     description="TBD",
@@ -12,13 +13,12 @@ setup(packages=find_packages(),
     author="Yubin Park",
     author_email="yubin.park@gmail.com",
     url="https://github.com/yubin-park/califorest",
-    license="Apaceh 2.0", 
-    install_requires = ["numpy", "sklearn"],
+    license="Apaceh 2.0",
+    install_requires=["numpy", "scikit-learn"],
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent"
-    ])
-
-
+        "Operating System :: OS Independent",
+    ],
+)
